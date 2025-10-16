8b0447a92a92caf1f6c41c25fdb74967a609ddc3
AnnaPelagejevska <annapelagejevska@gmail.com>
2025-10-16 23:51:59 +0300

    ww

--- flask.py
+++ flask.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/data')
 def get_data():
-    return jsonify({"message": "Привет, данные успешно получены!"})
+    return jsonify({"message": "Привет, данные успешно полуены!"})
 
 if __name__ == "__main__":
     app.run(debug=True)
